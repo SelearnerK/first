90959828090986372e5adc717419e285f1247596
SelearnerK <zxk19990425@163.com>
2021-10-09 21:33:08 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--img alt="Vue logo" src="./assets/logo.png"-->
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <my-first></my-first>
+<!--    <el-test></el-test>-->
+    <!--<router-link to="/my">myfirst</router-link>
+    <router-link to="/hello">hello</router-link>
+    <router-view></router-view>-->
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import MyFirst from "@/components/myFirst";
+//import elTest from "@/components/sonComponents/elTest";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MyFirst,
+    //HelloWorld,
+    // elTest
   }
 }
 </script>
