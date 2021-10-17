dd65486be315072efa93b0655ab1c570b3aac985
flybird <zxk19990425@163.com>
2021-10-17 21:42:12 +0800

    stepshowV1.1

--- src/components/stepShow.vue
+++ src/components/stepShow.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
-    <el-steps :active="active" finish-status="success">
-      <el-step title="步骤 1"></el-step>
-      <el-step title="步骤 2"></el-step>
-      <el-step title="步骤 3"></el-step>
+
+    <el-steps :active="active" align-center finish-status="success">
+      <el-step title="步骤1" description="这是一段很长很长很长的描述性文字"></el-step>
+      <el-step title="步骤2" description="这是一段很长很长很长的描述性文字"></el-step>
+      <el-step title="步骤3" description="这是一段很长很长很长的描述性文字"></el-step>
+      <el-step title="步骤4" description="这是一段很长很长很长的描述性文字"></el-step>
     </el-steps>
 
-    <el-button style="margin-top: 12px;" @click="next">下一步</el-button>
+    <el-button style="margin-top: 12px;" @click="next">{{ BtnMessage }}</el-button>
+    <el-button style="margin-top: 12px;" @click="back">{{ BtnMessage2 }}</el-button>
 
 
   </div>
@@ -18,12 +21,19 @@ export default {
   name: "stepShow",
   data(){
     return {
-      active:0
+      active:0,
+      BtnMessage:"下一步",
+      BtnMessage2:"上一步"
     };
   },
   methods:{
     next(){
-      if (this.active++ > 2) this.active=0
+      if (this.active++ >= 2) this.BtnMessage="完成"
+    },
+    back(){
+      this.BtnMessage="下一步"
+      if (this.active-- <=0) this.active=0;
+      /*在此处切换页面*/
     }
   }
 }
